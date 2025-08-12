414a24eebefb4741871d477d182e86f32255d886
ArthurWalker <whitefang17996@gmail.com>
2025-08-12 10:48:06 +0100

    Improve BraceExpansion

--- Stripe/Exercises/BraceExpansion.py
+++ Stripe/Exercises/BraceExpansion.py
@@ -1,50 +1,22 @@
 from typing import List
 
-# def convert_word(s:str) -> List[str]:
-#     result = []
-#     i = 0
-#     while i < len(s):
-#         if s[i] == '{':
-#             end = s.find('}', i)
-#             if end == -1:
-#                 raise ValueError("Mismatched braces")
-#             group = s[i+1:end].split(',')
-#             result.append(group)
-#             i = end + 1
-#         else:
-#             result.append([s[i]])
-#             i += 1
-#     return result
-
-
-def convert_word(word_list:str) -> List[str]:
-    list_word = []
-    i = 0
-    sublist = []
-    in_bracket = False
-    while i < len(word_list):
-        if word_list[i] == '{':
-            if len(sublist) > 0:
-                list_word.append(sublist)
-            sublist = []
-            in_bracket = True
-        elif word_list[i] == '}':
-            list_word.append(sublist)
-            sublist = []
-            in_bracket = False
+def convert_word(input):
+    stack = []
+    txt = ''
+    for ele in input:
+        if ele == '{':
+            if len(txt)>0:
+                stack.append([txt])
+            txt = ''
+        elif ele == '}':
+            txt_split =  txt.split(',')
+            stack.append(txt_split)
+            txt = ''
         else:
-            if word_list[i] != ',':
-                if not in_bracket:
-                    temp = sublist.pop() if len(sublist)> 0 else ""
-                    list_word.append(temp+word_list[i])
-                else:
-                    sublist.append(word_list[i])
-        i += 1
-    if len(sublist) > 0:
-        list_word.append(sublist)
-    return list_word
-
-
+            txt+=ele
+    if len(txt)>0:
+        stack.append([txt])
+    return stack
 
 def brace_expansion(word_list: str) -> List[str]:
     # {a,b,c}d{e,f} => [[a,b,c],[d],[e,f]]
