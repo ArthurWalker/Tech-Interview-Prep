8b85be17294418541952f7d2d4c1e10436265970
ArthurWalker <whitefang17996@gmail.com>
2025-09-09 22:58:07 +0100

    BinaryTree

---
+++ LeetcodePractices/BinaryTreePath.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        res = []
+        def dfs(node,path):
+            if not node.left and not node.right:
+                path_arrow = '->'.join(path+[str(node.val)])
+                if path_arrow not in res:
+                    res.append(path_arrow)
+                return 
+            if node.left:
+                dfs(node.left,path+[str(node.val)])
+            if node.right:
+                dfs(node.right,path+[str(node.val)])
+            return
+        dfs(root,[])
+        return (res)
