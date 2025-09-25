8a4852433fd15b5b83008bbca82c5e7f6cc9b06c
ArthurWalker <whitefang17996@gmail.com>
2025-09-25 22:56:23 +0100

    Improve time complexity

--- Microsoft/30days/maxidiffodd_even.py
+++ Microsoft/30days/maxidiffodd_even.py
@@ -2,7 +2,13 @@ class Solution:
     def maxDifference(self, s: str) -> int:
         set_a = set(s)
         fre_lett = {letter:s.count(letter) for letter in set_a}
-        odd_lst = sorted([num for num in fre_lett.values() if num %2 !=0])
-        even_lst =sorted([num for num in fre_lett.values() if num %2 ==0])
-        return odd_lst[-1]-even_lst[0]
+        even_lst = []
+        odd_lst = []
+        for num in fre_lett.values():
+            if num%2 == 0:
+                even_lst.append(num)
+            else:
+                odd_lst.append(num)
+        
+        return max(odd_lst)-min(even_lst)
 
