ca2ebc20bbbd5fbc9a7d36d896c2f1375ffb8f35
ArthurWalker <whitefang17996@gmail.com>
2025-09-25 22:36:01 +0100

    Add Bottom up

--- Microsoft/30days/ClimbStair.py
+++ Microsoft/30days/ClimbStair.py
@@ -14,3 +14,15 @@ class Solution:
             return dp[cur_step]
         
         return dfs(0)
+    
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        dp = [0]*(n+1)
+        
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3,n+1):
+            dp[i] = dp[i-1]+dp[i-2]
+        return dp[n]
