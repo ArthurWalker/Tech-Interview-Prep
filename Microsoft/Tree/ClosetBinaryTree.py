5ce2a6520ea97e8100e7cca8955abef8ff8f6049
ArthurWalker <whitefang17996@gmail.com>
2025-10-05 12:05:58 +0100

    Update SOlution ClosetBinaryTree

--- Microsoft/Tree/ClosetBinaryTree.py
+++ Microsoft/Tree/ClosetBinaryTree.py
@@ -6,16 +6,19 @@
 #         self.right = right
 class Solution:
     def closestValue(self, root: Optional[TreeNode], target: float) -> int:
-        if not root:
-            return 0
+        closet = root.val
         
         while root:
-            print(root.val)
-            if root.val < target:
-                return root.val
+            curr_diff = abs(root.val-target)
+            closet_diff = abs(closet - target)
+            if curr_diff < closet_diff or (curr_diff == closet_diff and root.val < closet):
+                closet = root.val
+
             if target >= root.val:
                 root = root.right
             else:
                 root = root.left
+        
+        return closet
         
         
